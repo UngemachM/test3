bc930687cae86b20f1033f6f88c249740b069630
Alex <hamidreza.rahimian@emerson.com>
2024-09-11 15:08:21 +0200

    linking and editing CSS and JS of Dashboard

--- public/dashboardAdmin/Dashboardadmin.js
+++ public/dashboardAdmin/Dashboardadmin.js
@@ -3,6 +3,8 @@ document.getElementById('new-project-link').addEventListener('click', function (
     document.getElementById('new-project-container').style.display = 'block';
     document.getElementById('main-container').style.display = 'none';
     document.getElementById('edit-project-container').style.display = 'none'; // hide edit project section
+    document.getElementById('edit-input-project-container').style.display = 'none'; // hide edit project section
+
 });
 
 // JavaScript to go back to "Projects Overview"
@@ -10,6 +12,7 @@ document.getElementById('overview-link').addEventListener('click', function () {
     document.getElementById('new-project-container').style.display = 'none';
     document.getElementById('main-container').style.display = 'block';
     document.getElementById('edit-project-container').style.display = 'none'; // hide edit project section
+    document.getElementById('edit-input-project-container').style.display = 'none'; // hide edit project section
     restoreDetailsButtons();
 });
 
@@ -18,6 +21,8 @@ document.getElementById('edit-project-link').addEventListener('click', function 
     document.getElementById('new-project-container').style.display = 'none'; // hide new project section
     document.getElementById('main-container').style.display = 'none'; // hide main project section
     document.getElementById('edit-project-container').style.display = 'block'; // show edit project section
+    document.getElementById('edit-input-project-container').style.display = 'none'; // hide edit project section
+
 });
 
 // Restore "View Details" buttons when switching back to Projects Overview
@@ -33,6 +38,7 @@ function restoreDetailsButtons() {
         document.getElementById('new-project-container').style.display = 'none'; // hide new project section
         document.getElementById('main-container').style.display = 'none'; // hide main project section
         document.getElementById('edit-project-container').style.display = 'none'; // show edit project section
+        document.getElementById('edit-input-project-container').style.display = 'none'; // hide edit project section
     });
 
 
